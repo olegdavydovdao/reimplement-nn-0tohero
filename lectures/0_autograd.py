9aed5191e0946ecad70f574b1622a7c6e5065ef7
oleg.davydov.dao <oleg.davydov.dao@gmail.com>
2026-04-14 18:09:43 +0300

    class MLP

--- lectures/0_autograd.py
+++ lectures/0_autograd.py
@@ -86,3 +86,36 @@ class Value:
         self.grad = 1.0
         for node in reversed(topo):
             node._backward()
+
+# PART 1: NN MLP LIBRARY ON TOP VALUE ENGINE
+# Implement MLP
+class Neuron:
+    def __init__(self, nin):
+        self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+        self.b = Value(random.uniform(-1,1))
+    def __call__(self, x):
+        act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+        out = act.tanh()
+        return out
+    def parameters(self):
+        return self.w + [self.b]
+
+class Layer:
+    def __init__(self, nin, nout):
+        self.neurons = [Neuron(nin) for _ in range(nout)]
+    def __call__(self,x):
+        out = [n(x) for n in self.neurons]
+        return out[0] if len(out) == 1 else out
+    def parameters (self):
+        return [p for neuron in self.neurons for p in neuron.parameters()]
+
+class MLP:
+    def __init__(self, nin, listout):
+        sz = [nin] + listout
+        self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(listout))]
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        return x
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
