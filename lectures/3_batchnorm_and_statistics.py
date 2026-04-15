9ff5c016422cd7eb70b00f64beaf603103bb6761
oleg.davydov.dao <oleg.davydov.dao@gmail.com>
2026-04-15 23:38:16 +0300

    lecture 3 pytorchifying

--- lectures/3_batchnorm_and_statistics.py
+++ lectures/3_batchnorm_and_statistics.py
@@ -56,3 +56,75 @@ print(f'total names: {len(names)}')
 print(f'bigram training examples: {num_tr}')
 print(f'bigram validation examples: {Yval.shape[0]}')
 print(f'bigram test examples: {Yte.shape[0]}')
+
+# PART 0: PYTORCHIFYING CODE AND INITIALIZATION
+# Build modules of nn
+class Linear:
+    def __init__(self, fan_in, fan_out, bias=True):
+        self.weight = torch.randn((fan_in, fan_out), generator = g)/fan_in**0.5
+        self.bias = torch.zeros(fan_out) if bias else None
+    def __call__(self, x):
+        self.out = x@self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
+    def parameters(self):
+        return [self.weight]+([] if self.bias is None else [self.bias])
+
+class BatchNorm1d:
+    def __init__(self, num_features, momentum=0.1, eps=1e-5):
+        self.training = True
+        self.eps = eps
+        self.momentum = momentum
+        self.gamma = torch.ones(num_features)
+        self.beta = torch.zeros(num_features)
+        self.running_mean = torch.zeros(num_features)
+        self.running_var = torch.ones(num_features)
+    def __call__(self, x):
+        if self.training:
+            xmean = x.mean(0)
+            xvar = x.var(0)
+            self.running_mean = (1-self.momentum)*self.running_mean + self.momentum * xmean
+            self.running_var = (1-self.momentum)*self.running_var + self.momentum * xvar
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+        xhat = (x - xmean)/(xvar+self.eps)**0.5
+        self.out = self.gamma * xhat + self.beta
+        return self.out
+    def parameters(self):
+        return [self.gamma, self.beta]
+        
+class Tanh:
+    def __call__(self,x):
+        self.out = torch.tanh(x)
+        return self.out
+    def parameters(self):
+        return []
+    
+# Init model's layers as modules in stack
+g = torch.Generator().manual_seed(2147483647)
+C = torch.randn((sz_voc, n_emb), generator = g)
+layers = [
+    Linear(n_emb*block_size, n_hidden, bias=False), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden, bias=False), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden, bias=False), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden, bias=False), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, n_hidden, bias=False), BatchNorm1d(n_hidden), Tanh(),
+    Linear(n_hidden, sz_voc, bias=False), BatchNorm1d(sz_voc),
+]
+# Parameters and logs
+parameters = [C] + [p for layer in layers for p in layer.parameters()]
+for p in parameters:
+    p.requires_grad = True
+print(f'num parameters: {sum(p.nelement() for p in parameters)}')
+ud = []
+
+# Adjust initialization
+with torch.no_grad():
+    # layers[-1].weight *= 0.1
+    layers[-1].gamma *= 0.1
+
+    for layer in layers[:-1]:
+        if isinstance(layer,Linear):
+            layer.weight *= 5/3
