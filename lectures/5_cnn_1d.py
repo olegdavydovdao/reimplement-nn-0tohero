ae0657b445d4549834fd75d248a2d8f4a485be6f
oleg.davydov.dao <oleg.davydov.dao@gmail.com>
2026-04-17 12:45:29 +0300

    pytorchifying lecture 5

--- lectures/5_cnn_1d.py
+++ lectures/5_cnn_1d.py
@@ -20,3 +20,83 @@ n_consec = 2
 # Preprocess names.txt to feed into the model
 Xtr,Ytr,Xval,Yval,Xte,Yte,itos,stoi,sz_voc,num_tr = get_splits_names(block_size=block_size)
 print(f"Xtr.shape: {Xtr.shape}")
+
+# PART 1: PYTORCHIFYING CODE
+# Build modules of nn
+class Linear:
+    def __init__(self, fan_in, fan_out, bias=True):
+        self.weight = torch.randn((fan_in, fan_out))/fan_in**0.5
+        self.bias = torch.zeros(fan_out) if bias else None
+    def __call__(self, x):
+        self.out = x@self.weight
+        if self.bias is not None:
+            self.out += self.bias
+        return self.out
+    def parameters(self):
+        return [self.weight]+([] if self.bias is None else [self.bias])
+
+class BatchNorm1d:
+    def __init__(self, num_features, momentum=0.1, eps=1e-5):
+        self.training = True
+        self.eps = eps
+        self.momentum = momentum
+        self.gamma = torch.ones(num_features)
+        self.beta = torch.zeros(num_features)
+        self.running_mean = torch.zeros(num_features)
+        self.running_var = torch.ones(num_features)
+    def __call__(self, x):
+        if self.training:
+            if x.ndim == 2:
+                dim = 0
+            elif x.ndim == 3:
+                dim = (0,1)
+            xmean = x.mean(dim)
+            xvar = x.var(dim)
+            self.running_mean = (1-self.momentum)*self.running_mean + self.momentum * xmean
+            self.running_var = (1-self.momentum)*self.running_var + self.momentum * xvar
+        else:
+            xmean = self.running_mean
+            xvar = self.running_var
+        xhat = (x - xmean)/(xvar+self.eps)**0.5
+        self.out = self.gamma * xhat + self.beta
+        return self.out
+    def parameters(self):
+        return [self.gamma, self.beta]
+
+class Tanh:
+    def __call__(self,x):
+        self.out = torch.tanh(x)
+        return self.out
+    def parameters(self):
+        return []
+
+class Embedding:
+    def __init__(self, num_embeddings, embedding_dim):
+        self.weight = torch.randn((num_embeddings, embedding_dim))
+    def __call__(self, x):
+        self.out = self.weight[x]
+        return self.out
+    def parameters(self):
+        return [self.weight]
+class FlattenConsecutive:
+    def __init__(self, n):
+        self.n = n
+    def __call__(self, x):
+        B, T, C = x.shape
+        x = x.view(B, T//self.n, C*self.n)
+        if x.shape[1]==1:
+            x = x.squeeze(1)
+        self.out = x
+        return self.out
+    def parameters(self):
+        return []
+class Sequential:
+    def __init__(self, layers):
+        self.layers = layers
+    def __call__(self, x):
+        for layer in self.layers:
+            x = layer(x)
+        self.out = x
+        return self.out
+    def parameters(self):
+        return [p for layer in self.layers for p in layer.parameters()]
