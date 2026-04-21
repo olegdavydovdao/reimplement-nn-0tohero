89b215f283fd6e198031ec6ad9d36d8a51bdd847
Oleg Davydov <oleg.davydov.dao@gmail.com>
2026-04-21 17:27:56 +0300

    print train split type

--- lectures/7_tokenizer.py
+++ lectures/7_tokenizer.py
@@ -19,7 +19,9 @@ def text_choise(split):
     else:
         text = "Ｕｎｉｃｏｄｅ! 🅤🅝🅘🅒🅞🅓🅔‽ 🇺‌🇳‌🇮‌🇨‌🇴‌🇩‌🇪! 😄 The very name strikes fear and awe into the hearts of programmers worldwide. We all know we ought to “support Unicode” in our software (whatever that means—like using wchar_t for all the strings, right?). But Unicode can be abstruse, and diving into the thousand-page Unicode Standard plus its dozens of supplementary annexes, reports, and notes can be more than a little intimidating. I don’t blame programmers for still finding the whole thing mysterious, even 30 years after Unicode’s inception."
     return text
-text = text_choise('tiny')
+train_split = 'tiny'
+print(f"tokenizer trained on {train_split} dataset")
+text = text_choise(train_split)
 
 # PART 1: TOKENIZER TRAIN
 # Tokens separating with regex pattern
